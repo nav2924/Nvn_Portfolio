084c2d25f851cb5fc9d4afef613a0ee5694cd2f7
nav2924 <navi29012004@gmail.com>
2025-04-11 18:49:46 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className='text-3xl underline'>Welcome to my 3D portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
